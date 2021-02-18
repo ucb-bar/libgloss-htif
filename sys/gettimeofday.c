b1f69d06d625e7deb04fa077ac2135074f861e9b
Albert Ou <aou@eecs.berkeley.edu>
2021-02-18 12:37:20 -0800

    Implement rudimentary _gettimeofday()

--- sys/gettimeofday.c
+++ sys/gettimeofday.c
@@ -1,9 +1,16 @@
 #include <errno.h>
 #include <sys/time.h>
 #include "syscall.h"
+#include "encoding.h"
+
+#ifndef CLOCK_FREQ
+#define CLOCK_FREQ (500000000UL)
+#endif
 
 int _gettimeofday(struct timeval *tp, void *tzp)
 {
-    errno = ENOSYS;
-    return -1;
+    uint64_t t = rdcycle();
+    tp->tv_sec = t / CLOCK_FREQ;
+    tp->tv_usec = (t % CLOCK_FREQ) / (CLOCK_FREQ / 1000000U);
+    return 0;
 }
